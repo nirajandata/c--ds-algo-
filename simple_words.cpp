66ef8aa5a34055054cf357c24d1f0c789f027ade
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-06-04 19:49:12 +0545

    Create simple_words.cpp

---
+++ simple_words.cpp
@@ -0,0 +1,11 @@
+// very very simple question solution
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+string t;
+cin >> t;
+t[0]=toupper(t[0]);
+cout <<t;
+}
